46d6e50a2ad335b93166fe680d1f87ed98286878
Dalton Pettus <daltpettus@gmail.com>
2025-06-02 13:02:10 -0700

    add component JSXMapSerializer to Heading for PrismicRichText heading and styles

--- src/slices/Hero/index.tsx
+++ src/slices/Hero/index.tsx
@@ -1,10 +1,20 @@
 import { FC } from "react";
 import { Content } from "@prismicio/client";
-import { PrismicRichText, SliceComponentProps } from "@prismicio/react";
+import { JSXMapSerializer, PrismicRichText, SliceComponentProps } from "@prismicio/react";
 import Bounded from "@/components/Bounded";
 import { PrismicNextImage } from "@prismicio/next";
 import Button from "@/components/Button";
+import Heading from "@/components/Heading";
 // import styles from '@/styles/styles.module.css';
+
+const components: JSXMapSerializer = {
+  heading1: ({ children }) => (
+    <Heading as="h1" size="md" className="italic">
+      {children}
+    </Heading>
+  )
+  
+}
 
 /**
  * Props for `Hero`.
@@ -17,10 +27,10 @@ export type HeroProps = SliceComponentProps<Content.HeroSlice>;
 const Hero: FC<HeroProps> = ({ slice }) => {
   return (
     <Bounded data-slice-type={slice.slice_type} data-slice-variation={slice.variation} className="bg-[var(--site-secondary-color)]">
-      <div className="grid grid-cols-1 md:grid-cols-2 place-items-center w-8/12 m-auto">
-        <PrismicNextImage field={slice.primary.image} width={3259} height={4888} className="h-96 w-auto"/>
+      <div className="grid grid-cols-1 md:grid-cols-2 place-items-center w-10/12 m-auto">
+        <PrismicNextImage field={slice.primary.image} width={3259} height={4888} className="h-96 w-auto mx-12"/>
         <div className="text-center place-items-center">
-          <PrismicRichText field={slice.primary.heading} />
+          <PrismicRichText field={slice.primary.heading} components={components}/>
           <Button field={slice.primary.button} className="my-3"/>
         </div>
       </div>
