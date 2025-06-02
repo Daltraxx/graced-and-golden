e932fd6eff5363bfe94842c9ab78b7a00fe0c22b
Dalton Pettus <daltpettus@gmail.com>
2025-06-02 12:12:50 -0700

    add layout styles to Hero

--- src/slices/Hero/index.tsx
+++ src/slices/Hero/index.tsx
@@ -1,6 +1,10 @@
 import { FC } from "react";
 import { Content } from "@prismicio/client";
-import { SliceComponentProps } from "@prismicio/react";
+import { PrismicRichText, SliceComponentProps } from "@prismicio/react";
+import Bounded from "@/components/Bounded";
+import { PrismicNextImage } from "@prismicio/next";
+import Button from "@/components/Button";
+// import styles from '@/styles/styles.module.css';
 
 /**
  * Props for `Hero`.
@@ -12,39 +16,15 @@ export type HeroProps = SliceComponentProps<Content.HeroSlice>;
  */
 const Hero: FC<HeroProps> = ({ slice }) => {
   return (
-    <section
-      data-slice-type={slice.slice_type}
-      data-slice-variation={slice.variation}
-    >
-      Placeholder component for hero (variation: {slice.variation}) slices.
-      <br />
-      <strong>You can edit this slice directly in your code editor.</strong>
-      {/**
-       * 💡 Use Prismic MCP with your code editor
-       *
-       * Get AI-powered help to build your slice components — based on your actual model.
-       *
-       * ▶️ Setup:
-       * 1. Add a new MCP Server in your code editor:
-       *
-       * {
-       *   "mcpServers": {
-       *     "Prismic MCP": {
-       *       "command": "npx",
-       *       "args": ["-y", "@prismicio/mcp-server"]
-       *     }
-       *   }
-       * }
-       *
-       * 2. Select Claude 3.7 Sonnet (recommended for optimal output)
-       *
-       * ✅ Then open your slice file and ask your code editor:
-       *    "Code this slice"
-       *
-       * Your code editor reads your slice model and helps you code faster ⚡
-       * 📚 Give your feedback: https://community.prismic.io/t/help-us-shape-the-future-of-slice-creation/19505
-       */}
-    </section>
+    <Bounded data-slice-type={slice.slice_type} data-slice-variation={slice.variation}>
+      <div className="grid grid-cols-1 md:grid-cols-2 place-items-center">
+        <PrismicNextImage field={slice.primary.image} width={3259} height={4888} className="h-96 w-auto"/>
+        <div>
+          <PrismicRichText field={slice.primary.heading} />
+          <Button field={slice.primary.button} />
+        </div>
+      </div>
+    </Bounded>
   );
 };
 
