ae8816899ac5c65ad4b67dbdbbaa7385b9818ffb
Dalton Pettus <daltpettus@gmail.com>
2025-06-02 16:22:26 -0700

    Start coding Introduction component

--- src/slices/Introduction/index.tsx
+++ src/slices/Introduction/index.tsx
@@ -1,7 +1,19 @@
 import { FC } from "react";
 import { Content } from "@prismicio/client";
-import { SliceComponentProps } from "@prismicio/react";
+import { JSXMapSerializer, PrismicRichText, SliceComponentProps } from "@prismicio/react";
+import Bounded from "@/components/Bounded";
+import Heading from "@/components/Heading";
 
+const components : JSXMapSerializer = {
+  heading2: ({ children }) => (
+    <Heading as="h2" size="md" className="">
+      {children}
+    </Heading>
+  ),
+  paragraph: ({children}) => (
+    <p>{children}</p>
+  )
+}
 /**
  * Props for `Introduction`.
  */
@@ -12,40 +24,13 @@ export type IntroductionProps = SliceComponentProps<Content.IntroductionSlice>;
  */
 const Introduction: FC<IntroductionProps> = ({ slice }) => {
   return (
-    <section
-      data-slice-type={slice.slice_type}
-      data-slice-variation={slice.variation}
-    >
-      Placeholder component for introduction (variation: {slice.variation})
-      slices.
-      <br />
-      <strong>You can edit this slice directly in your code editor.</strong>
-      {/**
-       * 💡 Use Prismic MCP with your code editor
-       *
-       * Get AI-powered help to build your slice components — based on your actual model.
-       *
-       * ▶️ Setup:
-       * 1. Add a new MCP Server in your code editor:
-       *
-       * {
-       *   "mcpServers": {
-       *     "Prismic MCP": {
-       *       "command": "npx",
-       *       "args": ["-y", "@prismicio/mcp-server"]
-       *     }
-       *   }
-       * }
-       *
-       * 2. Select Claude 3.7 Sonnet (recommended for optimal output)
-       *
-       * ✅ Then open your slice file and ask your code editor:
-       *    "Code this slice"
-       *
-       * Your code editor reads your slice model and helps you code faster ⚡
-       * 📚 Give your feedback: https://community.prismic.io/t/help-us-shape-the-future-of-slice-creation/19505
-       */}
-    </section>
+    <Bounded data-slice-type={slice.slice_type} data-slice-variation={slice.variation} >
+      <PrismicRichText field={slice.primary.heading} components={components} />
+      <div>
+        <PrismicRichText field={slice.primary.paragraphs_column_1} components={components} />
+        <PrismicRichText field={slice.primary.paragraphs_column_2} components={components} />
+      </div>
+    </Bounded>
   );
 };
 
