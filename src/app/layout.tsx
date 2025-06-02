2b3cee6819eeeb00c6ea37830839e4d2550eca35
Dalton Pettus <daltpettus@gmail.com>
2025-06-01 21:12:27 -0700

    give html element in root layout background color defined in globals.css

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className={`${clsx(nunito.variable, nunitoSans.variable)} antialiased`}>
+    <html lang="en" className={`${clsx(nunito.variable, nunitoSans.variable)} antialiased bg-[var(--body-background-color)]`}>
       <body>
         {children}
       </body>
