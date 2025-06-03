4ba120113dded33fd1866c87897ff86ab3247834
Dalton Pettus <daltpettus@gmail.com>
2025-06-03 15:58:55 -0700

    style HeroThreeImage slice

--- src/slices/HeroThreeImage/index.tsx
+++ src/slices/HeroThreeImage/index.tsx
@@ -4,6 +4,8 @@ import { JSXMapSerializer, PrismicRichText, SliceComponentProps } from "@prismic
 import Bounded from "@/components/Bounded";
 import Heading from "@/components/Heading";
 import { PrismicNextImage } from "@prismicio/next";
+import styles from '@/styles/styles.module.css';
+import sliceStyles from '@/slices/HeroThreeImage/styles.module.css';
 
 const components: JSXMapSerializer = {
   heading1: ({children}) => (
@@ -29,12 +31,16 @@ export type HeroThreeImageProps =
  */
 const HeroThreeImage: FC<HeroThreeImageProps> = ({ slice }) => {
   return (
-    <Bounded data-slice-type={slice.slice_type} data-slice-variation={slice.variation} >
-      <PrismicRichText field={slice.primary.heading} />
-      <PrismicRichText field={slice.primary.body} />
-      <PrismicNextImage field={slice.primary.image_left} />
-      <PrismicNextImage field={slice.primary.image_center} />
-      <PrismicNextImage field={slice.primary.image_right} />
+    <Bounded data-slice-type={slice.slice_type} data-slice-variation={slice.variation} className={`${styles.backgroundCream300}`}>
+      <div className={`${sliceStyles.heroContentContainer}`}>
+        <div className={`${sliceStyles.textContentContainer}`}>
+          <PrismicRichText field={slice.primary.heading} components={components} />
+          <PrismicRichText field={slice.primary.body} components={components} />
+        </div>
+        <PrismicNextImage field={slice.primary.image_left} className={`${sliceStyles.heroImage} ${sliceStyles.hide}`} />
+        <PrismicNextImage field={slice.primary.image_center} className={`${sliceStyles.heroImage}`} />
+        <PrismicNextImage field={slice.primary.image_right} className={`${sliceStyles.heroImage}`} />
+      </div>
     </Bounded>
   );
 };
