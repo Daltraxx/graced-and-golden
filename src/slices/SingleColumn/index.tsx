5ecc4e84ad1d0ce10892953f684d4940817bcdb5
Dalton Pettus <daltpettus@gmail.com>
2025-06-05 16:35:37 -0700

    style SingleColumn

--- src/slices/SingleColumn/index.tsx
+++ src/slices/SingleColumn/index.tsx
@@ -1,10 +1,20 @@
 import { FC } from "react";
 import { Content } from "@prismicio/client";
-import { PrismicRichText, SliceComponentProps } from "@prismicio/react";
-import { PrismicNextLink } from "@prismicio/next";
+import { JSXMapSerializer, PrismicRichText, SliceComponentProps } from "@prismicio/react";
 import Bounded from "@/components/Bounded";
 import styles from '@/styles/styles.module.css';
 import moduleStyles from '@/slices/SingleColumn/styles.module.css';
+import Heading from "@/components/Heading";
+import Button from "@/components/Button";
+
+const components : JSXMapSerializer = {
+  heading2: ({children}) => (
+    <Heading as="h2" size="md" className="my-4">{children}</Heading>
+  ),
+  paragraph: ({children}) => (
+    <p className="">{children}</p>
+  )
+}
 
 /**
  * Props for `SingleColumn`.
@@ -16,12 +26,18 @@ export type SingleColumnProps = SliceComponentProps<Content.SingleColumnSlice>;
  */
 const SingleColumn: FC<SingleColumnProps> = ({ slice }) => {
   return (
-    <Bounded data-slice-type={slice.slice_type} data-slice-variation={slice.variation} className={`${styles.backgroundCream300}`}>
+    <Bounded data-slice-type={slice.slice_type} data-slice-variation={slice.variation} className={`${styles.backgroundCream200}`}>
       <div style={{ backgroundImage: `url(${slice.primary.background_image.url})`}} className={`${moduleStyles.container}`}>
-        <PrismicRichText field={slice.primary.heading} />
-        <PrismicRichText field={slice.primary.body} />
-        <PrismicNextLink field={slice.primary.link_left} />
-        <PrismicNextLink field={slice.primary.link_right} />
+        <PrismicRichText field={slice.primary.heading} components={components}/>
+        <PrismicRichText field={slice.primary.body} components={components}/>
+        <ul className={`${moduleStyles.buttonContainer}`}>
+          <li>
+            <Button field={slice.primary.link_left} className={`${styles.buttonCream200} ${moduleStyles.button}`} />
+          </li>
+          <li>
+            <Button field={slice.primary.link_right} className={`${styles.buttonCream200} ${moduleStyles.button}`} />
+          </li>
+        </ul>
       </div>
     </Bounded>
   );
