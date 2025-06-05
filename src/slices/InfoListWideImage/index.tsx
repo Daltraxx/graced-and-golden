f35fe5654c91f3911de5ff7956ae0a120a0aaf66
Dalton Pettus <daltpettus@gmail.com>
2025-06-05 12:43:37 -0700

    begin styling InfoListWideImage slice

--- src/slices/InfoListWideImage/index.tsx
+++ src/slices/InfoListWideImage/index.tsx
@@ -1,6 +1,28 @@
 import { FC } from "react";
 import { Content } from "@prismicio/client";
-import { SliceComponentProps } from "@prismicio/react";
+import { JSXMapSerializer, PrismicRichText, SliceComponentProps } from "@prismicio/react";
+import Heading from "@/components/Heading";
+import Bounded from "@/components/Bounded";
+import { PrismicNextImage } from "@prismicio/next";
+import styles from '@/styles/styles.module.css';
+import moduleStyles from '@/slices/InfoListWideImage/styles.module.css'
+
+const components: JSXMapSerializer = {
+  heading2: ({children}) => (
+    <Heading as="h2" size="lg" className="italic">
+        {children}
+    </Heading>
+  ),
+  heading3: ({children}) => (
+    <Heading as="h3" size="sm" className="">
+        {children}
+    </Heading>
+  ),
+  list: ({children}) => (
+    <ul className={`${moduleStyles.list}`}>{children}</ul>
+  )
+  
+}
 
 /**
  * Props for `InfoListWideImage`.
@@ -13,40 +35,11 @@ export type InfoListWideImageProps =
  */
 const InfoListWideImage: FC<InfoListWideImageProps> = ({ slice }) => {
   return (
-    <section
-      data-slice-type={slice.slice_type}
-      data-slice-variation={slice.variation}
-    >
-      Placeholder component for info_list_wide_image (variation:{" "}
-      {slice.variation}) slices.
-      <br />
-      <strong>You can edit this slice directly in your code editor.</strong>
-      {/**
-       * 💡 Use Prismic MCP with your code editor
-       *
-       * Get AI-powered help to build your slice components — based on your actual model.
-       *
-       * ▶️ Setup:
-       * 1. Add a new MCP Server in your code editor:
-       *
-       * {
-       *   "mcpServers": {
-       *     "Prismic MCP": {
-       *       "command": "npx",
-       *       "args": ["-y", "@prismicio/mcp-server"]
-       *     }
-       *   }
-       * }
-       *
-       * 2. Select Claude 3.7 Sonnet (recommended for optimal output)
-       *
-       * ✅ Then open your slice file and ask your code editor:
-       *    "Code this slice"
-       *
-       * Your code editor reads your slice model and helps you code faster ⚡
-       * 📚 Give your feedback: https://community.prismic.io/t/help-us-shape-the-future-of-slice-creation/19505
-       */}
-    </section>
+    <Bounded data-slice-type={slice.slice_type} data-slice-variation={slice.variation}>
+      <PrismicNextImage field={slice.primary.background_image} />
+      <PrismicRichText field={slice.primary.heading} components={components} />
+      <PrismicRichText field={slice.primary.list} components={components} />
+    </Bounded>
   );
 };
 
