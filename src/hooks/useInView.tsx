948417669babe80c46e8277e36196a5e24add80a
Dalton Pettus <daltpettus@gmail.com>
2025-06-08 12:25:13 -0700

    create useInView hook

---
+++ src/hooks/useInView.tsx
@@ -0,0 +1,30 @@
+import { useState, useEffect, useRef } from 'react';
+
+const useInView = (options: { threshold: number }) => {
+   const [isInView, setIsInView] = useState(false);
+   const elementRef = useRef(null);
+
+   useEffect(() => {
+      const intersectionCallback = (entries: IntersectionObserverEntry[]) => {
+         entries.forEach((entry) => {
+         setIsInView(entry.isIntersecting);
+         })
+      }
+
+      const observer = new IntersectionObserver(intersectionCallback, options);
+
+      if (elementRef.current) {
+         observer.observe(elementRef.current);
+      }
+
+      const cleanup = () => {
+         if (elementRef.current) observer.unobserve(elementRef.current);
+      }
+
+      return cleanup;
+   }, [options]);
+
+   return [elementRef, isInView];
+}
+
+export default useInView;
