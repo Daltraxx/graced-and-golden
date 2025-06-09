7bf123ff8c265feb2041d60f0a1b2ee1950457e4
Dalton Pettus <daltpettus@gmail.com>
2025-06-09 11:53:41 -0700

    get animation working on Triptych

--- src/slices/Tryptich/index.tsx
+++ src/slices/Tryptich/index.tsx
@@ -1,4 +1,6 @@
-import { FC } from "react";
+'use client';
+
+import { FC, useEffect } from "react";
 import { Content } from "@prismicio/client";
 import { JSXMapSerializer, PrismicRichText, SliceComponentProps } from "@prismicio/react";
 import Bounded from "@/components/Bounded";
@@ -6,6 +8,7 @@ import { PrismicNextImage, PrismicNextLink } from "@prismicio/next";
 import styles from '@/styles/styles.module.css';
 import moduleStyles from '@/slices/Tryptich/styles.module.css';
 import Heading from "@/components/Heading";
+import useInView from "@/hooks/useInView";
 
 const components: JSXMapSerializer = {
   heading2: ({ children }) => (
@@ -29,9 +32,17 @@ export type TryptichProps = SliceComponentProps<Content.TryptichSlice>;
  */
 const Tryptich: FC<TryptichProps> = ({ slice }) => {
   
+  const [elementRef, isInView] = useInView({ threshold: .4 });
+
+  useEffect(() => {
+    if (isInView) {
+      document.querySelector('.animated-element')?.classList.add('fade-in');
+    }
+  }, [isInView])
+
   return (
     <Bounded data-slice-type={slice.slice_type} data-slice-variation={slice.variation} className={`${styles.backgroundGradientBrown}`}>
-      <div className={`${moduleStyles.row}`}>
+      <div ref={elementRef} className={`${moduleStyles.row} animated-element`}>
         <div style={{ backgroundImage: `url(${slice.primary.image_left.url})`}} className={`${moduleStyles.bgImageContainer}`}></div>
         <div className={`${moduleStyles.bodyContainer}`}>
           <section className={`${moduleStyles.bodyText}`}>
