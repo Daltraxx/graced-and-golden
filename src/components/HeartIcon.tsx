e2f4a27467ff58907cd47cbc9abf21c5497b33eb
Dalton Pettus <daltpettus@gmail.com>
2025-06-11 18:51:42 -0700

    add brown heart icon svg

---
+++ src/components/HeartIcon.tsx
@@ -0,0 +1,13 @@
+const HeartIcon = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="100"
+    height="100"
+    fill="#A8887A"
+    viewBox="0 0 24 24"
+  >
+    <path d="M12 21.4 10.6 20C5.4 15.4 2 12.3 2 8.5 2 5.5 4.4 3 7.5 3A6 6 0 0 1 12 5a6 6 0 0 1 4.5-2c3 0 5.5 2.4 5.5 5.5 0 3.8-3.4 6.9-8.6 11.5z"></path>
+  </svg>
+);
+
+export default HeartIcon;
