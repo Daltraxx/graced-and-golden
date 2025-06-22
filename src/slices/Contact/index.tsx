6e7015466139a861784b00934cc369cab4987287
Dalton Pettus <daltpettus@gmail.com>
2025-06-22 13:57:09 -0700

    add some initial styles to Contact

--- src/slices/Contact/index.tsx
+++ src/slices/Contact/index.tsx
@@ -2,6 +2,8 @@ import { FC } from "react";
 import { Content } from "@prismicio/client";
 import { JSXMapSerializer, PrismicRichText, SliceComponentProps } from "@prismicio/react";
 import Heading from "@/components/Heading";
+import styles from '@/slices/Contact/styles.module.css';
+import Bounded from "@/components/Bounded";
 
 const components: JSXMapSerializer = {
   heading2: ({ children }) => (
@@ -10,7 +12,7 @@ const components: JSXMapSerializer = {
     </Heading>
   ),
   heading3: ({ children }) => (
-    <Heading as="h3" size="sm" className={``}>
+    <Heading as="h3" size="sm" className={`mb-4`}>
       {children}
     </Heading>
   ),
@@ -29,43 +31,62 @@ export type ContactProps = SliceComponentProps<Content.ContactSlice>;
  */
 const Contact: FC<ContactProps> = ({ slice }) => {
   return (
-    <section data-slice-type={slice.slice_type} data-slice-variation={slice.variation} >
+    <Bounded data-slice-type={slice.slice_type} data-slice-variation={slice.variation} >
       <section>
-        <PrismicRichText field={slice.primary.form_heading} />
-        <form action="">
-          <label htmlFor="name-field">{slice.primary.name_prompt}</label>
-          <input type="text" name="name" id="name-field" />
+        <PrismicRichText field={slice.primary.form_heading} components={components} />
+        <form action="" className={`${styles.inquiryForm}`}>
+          <div>
+            <label htmlFor="name-field">{slice.primary.name_prompt}</label>
+            <input type="text" name="name" id="name-field" />
+          </div>
 
-          <label htmlFor="phone-number-field">{slice.primary.phone_number_prompt}</label>
-          <input type="text" name="phone-number" id="phone-number-field" />
+          <div>
+            <label htmlFor="phone-number-field">{slice.primary.phone_number_prompt}</label>
+            <input type="text" name="phone-number" id="phone-number-field" />
+          </div>
 
-          <label htmlFor="email-field">{slice.primary.email_prompt}</label>
-          <input type="text" name="email" id="email-field" />
+          <div>
+            <label htmlFor="email-field">{slice.primary.email_prompt}</label>
+            <input type="text" name="email" id="email-field" />
+          </div>
 
-          <label htmlFor="birthday-field">{slice.primary.birthday_prompt}</label>
-          <input type="text" name="birthday" id="birthday-field" />
+          <div>
+            <label htmlFor="birthday-field">{slice.primary.birthday_prompt}</label>
+            <input type="text" name="birthday" id="birthday-field" />
+          </div>
 
-          <label htmlFor="instagram-field">{slice.primary.instagram_prompt}</label>
-          <input type="text" name="instagram" id="instagram-field" />
+          <div>
+            <label htmlFor="instagram-field">{slice.primary.instagram_prompt}</label>
+            <input type="text" name="instagram" id="instagram-field" />
+          </div>
 
-          <label htmlFor="occasion-field">{slice.primary.occasion_prompt}</label>
-          <input type="text" name="occasion" id="occasion-field" />
+          <div>
+            <label htmlFor="occasion-field">{slice.primary.occasion_prompt}</label>
+            <input type="text" name="occasion" id="occasion-field" />
+          </div>
 
-          <label htmlFor="how-found-field">{slice.primary.how_you_found_us_prompt}</label>
-          <input type="text" name="how-found" id="how-found-field" />
+          <div>
+            <label htmlFor="how-found-field">{slice.primary.how_you_found_us_prompt}</label>
+            <input type="text" name="how-found" id="how-found-field" />
+          </div>
 
-          <label htmlFor="tan-history-field">{slice.primary.tan_history_prompt}</label>
-          <input type="text" name="tan-history" id="tan-history-field" />
+          <div>
+            <label htmlFor="tan-history-field">{slice.primary.tan_history_prompt}</label>
+            <input type="text" name="tan-history" id="tan-history-field" />
+          </div>
 
-          <label htmlFor="desired-results-field">{slice.primary.desired_results_prompt}</label>
-          <input type="text" name="desired-results" id="desired-results-field" />
+          <div>
+            <label htmlFor="desired-results-field">{slice.primary.desired_results_prompt}</label>
+            <input type="text" name="desired-results" id="desired-results-field" />
+          </div>
 
-          <label htmlFor="questions-concerns-field">{slice.primary.questions_and_concerns_prompt}</label>
-          <input type="text" name="questions-concerns" id="questions-concerns-field" />
-
+          <div>
+            <label htmlFor="questions-concerns-field">{slice.primary.questions_and_concerns_prompt}</label>
+            <input type="text" name="questions-concerns" id="questions-concerns-field" />
+          </div>
         </form>
       </section>
-    </section>
+    </Bounded>
   );
 };
 
