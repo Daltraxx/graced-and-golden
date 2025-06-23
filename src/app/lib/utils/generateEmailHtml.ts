55a292b45ac063b0c0a97a07cbb8a21d0c3680e2
Dalton Pettus <daltpettus@gmail.com>
2025-06-23 11:26:10 -0700

    add generateEmailHtml function module

---
+++ src/app/lib/utils/generateEmailHtml.ts
@@ -0,0 +1,39 @@
+export default function generateEmailHtml
+   (
+      name: string, phoneNumber: string, email: string, birthday: string, instagram: string,
+      occasion: string, howFound: string, tanHistory: string, desiredResults: string, 
+      questionsConcerns: string | null = 'N/A'
+   ): string {
+   let html = `
+      <p>Name:</p>
+      <p>${name}</p>
+      <br>
+      <p>Phone Number:</p>
+      <p>${phoneNumber}</p>
+      <br>
+      <p>Email:</p>
+      <p>${email}</p>
+      <br>
+      <p>Birthday:</p>
+      <p>${birthday}</p>
+      <br>
+      <p>Instagram Handle:</p>
+      <p>${instagram}</p>
+      <br>
+      <p>What is the occasion and when is it?:</p>
+      <p>${occasion}</p>
+      <br>
+      <p>How did you find Graced and Golden?:</p>
+      <p>${howFound}</p>
+      <br>
+      <p>Have you had a spray tan before? If so, when was your last?:</p>
+      <p>${tanHistory}</p>
+      <br>
+      <p>What are your desired tanning results?:</p>
+      <p>${desiredResults}</p>
+   `;
+
+   if (questionsConcerns) html += `<br><p>List any questions/concerns:</p><p>${questionsConcerns}</p>`;
+
+   return html;
+}
