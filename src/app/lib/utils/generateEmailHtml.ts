21770d4b452756f0cdccc8d5141a6ec393cd9afc
Dalton Pettus <daltpettus@gmail.com>
2025-06-23 12:05:58 -0700

    modify generateEmailHtml to take in single object parameter with appropriate type

--- src/app/lib/utils/generateEmailHtml.ts
+++ src/app/lib/utils/generateEmailHtml.ts
@@ -1,41 +1,42 @@
-export default function generateEmailHtml
-(
+type FormInfo = {
    name: string, phoneNumber: string, email: string, birthday: string, instagram: string,
    occasion: string, howFound: string, tanHistory: string, desiredResults: string, 
    questionsConcerns: string
-): string {
-   if (!questionsConcerns) questionsConcerns = 'N/A';
+}
+
+export default function generateEmailHtml(formInfo : FormInfo): string {
+   if (!formInfo.questionsConcerns) formInfo.questionsConcerns = 'N/A';
    
    let html = `
       <p>Name:</p>
-      <p>${name}</p>
+      <p>${formInfo.name}</p>
       <br>
       <p>Phone Number:</p>
-      <p>${phoneNumber}</p>
+      <p>${formInfo.phoneNumber}</p>
       <br>
       <p>Email:</p>
-      <p>${email}</p>
+      <p>${formInfo.email}</p>
       <br>
       <p>Birthday:</p>
-      <p>${birthday}</p>
+      <p>${formInfo.birthday}</p>
       <br>
       <p>Instagram Handle:</p>
-      <p>${instagram}</p>
+      <p>${formInfo.instagram}</p>
       <br>
       <p>What is the occasion and when is it?:</p>
-      <p>${occasion}</p>
+      <p>${formInfo.occasion}</p>
       <br>
       <p>How did you find Graced and Golden?:</p>
-      <p>${howFound}</p>
+      <p>${formInfo.howFound}</p>
       <br>
       <p>Have you had a spray tan before? If so, when was your last?:</p>
-      <p>${tanHistory}</p>
+      <p>${formInfo.tanHistory}</p>
       <br>
       <p>What are your desired tanning results?:</p>
-      <p>${desiredResults}</p>
+      <p>${formInfo.desiredResults}</p>
       <br>
       <p>List any questions/concerns:</p>
-      <p>${questionsConcerns}</p>
+      <p>${formInfo.questionsConcerns}</p>
    `;
 
    return html;
