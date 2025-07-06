41e19f6f48fefb929c5370255a1fcf0b3411397a
Dalton Pettus <daltpettus@gmail.com>
2025-07-06 10:57:18 -0700

    add color prop to button to control styles

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -2,15 +2,24 @@ import { PrismicNextLink, PrismicNextLinkProps } from "@prismicio/next";
 import clsx from "clsx";
 import styles from '@/styles/styles.module.css'
 
+type ButtonProps = PrismicNextLinkProps & {
+   color?: 'brown-200' | 'brown-300' | 'brown-500';
+}
+
 
 export default function Button({
    className,
+   color = 'brown-200',
    ...restProps
-}: PrismicNextLinkProps) {
+}: ButtonProps) {
 
    return (
       <PrismicNextLink 
-         className={clsx(styles.button, className)}
+         className={clsx(styles.button,
+            color === 'brown-200' && styles.buttonBrown200,
+            color === 'brown-300' && styles.buttonBrown300,
+            color === 'brown-500' && styles.buttonBrown500,
+            className)}
          {...restProps}
       />
    );
