563531d9ea6db34ef5bfac181ea37b08223e423b
Dalton Pettus <daltpettus@gmail.com>
2025-07-06 17:45:05 -0700

    move tailwind styles to css module

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -5,25 +5,26 @@ import Link from "next/link";
 import Bounded from "../Bounded";
 import Button from "../Button";
 import styles from '@/styles/styles.module.css';
+import moduleStyles from '@/components/Header/styles.module.css';
 
 export default async function Header() {
    const client = createClient();
    const header = await client.getSingle('header');
 
    return (
-      <Bounded as='header' className="bg-[var(--brown-200)] font-display">
-         <div className="flex gap-4 items-center justify-between flex-col md:flex-row">
+      <Bounded as='header' className={`${styles.backgroundBrown200} ${moduleStyles.headerBounded}`}>
+         <div className={`${moduleStyles.headerContainer}`}>
             <Link href='/'>
                <Image src='/logo-full-header.png' width={100} height={54} alt='Graced and Golden logo'/>
             </Link>
-            <ul className="flex flex-col sm:flex-row gap-4 place-items-center">
+            <ul className={`${moduleStyles.linksContainer}`}>
                {header.data.nav_link.map((link) => (
                   <li key={link.key}>
                      <PrismicNextLink key={link.key} field={link} />
                   </li>
                ))}
                <li>
-                  <Button field={header.data.booking_link} className={styles.buttonCream200}/>
+                  <Button field={header.data.booking_link} className={styles.buttonCream200} color="cream-200"/>
                </li>
             </ul>
          </div>
