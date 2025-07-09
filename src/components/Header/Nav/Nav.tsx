8669834f39588aefc7ac0d80b8e0d65709933d4d
Dalton Pettus <daltpettus@gmail.com>
2025-07-09 13:37:34 -0700

    move services dropdown menu to own component, be careful of mistakes

--- src/components/Header/Nav/Nav.tsx
+++ src/components/Header/Nav/Nav.tsx
@@ -5,60 +5,21 @@ import { useState } from "react";
 import moduleStyles from '@/components/Header/Nav/styles.module.css'
 import { PrismicNextLink } from "@prismicio/next";
 import { LinkField } from "@prismicio/client";
+import ServicesMenu from "./ServicesMenu/ServicesMenu";
 
 export default function Nav({ navLinks, servicePageLinks }: { navLinks: LinkField[], servicePageLinks: LinkField[] }) {
    const [navOpen, setNavOpen] = useState(false);
-   const [servicesOpen, setServicesOpen] = useState(false);
 
    const handleNavToggleClick = () => {
       setNavOpen(prev => !prev);
    }
-
-   const handleServicesToggle = () => {
-      setServicesOpen(prev => !prev);
-   }
-
-   const servicesDropdownListItems = servicePageLinks.map((link, i) => (
-      <li key={`service-dropdown-link-${i}`} >
-         <PrismicNextLink field={link} className={moduleStyles.serviceLink} />
-      </li>
-   ))
 
    const navListItems = navLinks.map((link, i) => {
       // consider changing comparison to be based on something else such as slug or uid
       if (link.text && link.text.toLowerCase() === 'services') {
          return (
             <li key={`nav-link-${i}`}>
-               <button
-                  type="button"
-                  onClick={handleServicesToggle}
-                  className={clsx(moduleStyles.mainServiceButton,
-                     servicesOpen && moduleStyles.mainServiceButtonOpenState,
-                     !servicesOpen && moduleStyles.mainServiceButtonClosedState
-                  )}
-               >
-                  {link.text}
-                  <span
-                     className={clsx(
-                        moduleStyles.menuArrow,
-                        moduleStyles.serviceMenuArrow,
-                        servicesOpen && moduleStyles.menuArrowDown,
-                        !servicesOpen && moduleStyles.menuArrowUp
-                     )}
-                  >
-                  </span>
-               </button>
-               <div
-                  className={clsx(
-                        moduleStyles.servicesContainer,
-                        servicesOpen && moduleStyles.servicesDisplayed,
-                        !servicesOpen && moduleStyles.servicesHidden
-                     )}
-               >
-                  <ul className={moduleStyles.servicesList}>
-                     {servicesDropdownListItems}
-                  </ul>
-               </div>
+               <ServicesMenu text={link.text} servicePageLinks={servicePageLinks} />
             </li>
          );
       } else {
--- src/components/Header/Nav/ServicesMenu/ServicesMenu.tsx
+++ src/components/Header/Nav/ServicesMenu/ServicesMenu.tsx
@@ -1,1 +1,56 @@
+'use client';
 
+import { PrismicNextLink } from "@prismicio/next";
+import { useState } from "react";
+import moduleStyles from '@/components/Header/Nav/ServicesMenu/styles.module.css'
+import clsx from "clsx";
+import { LinkField } from "@prismicio/client";
+
+export default function ServicesMenu({ text, servicePageLinks }: { text: string, servicePageLinks: LinkField[] }) {
+   const [servicesOpen, setServicesOpen] = useState(false);
+
+   const handleServicesToggle = () => {
+      setServicesOpen(prev => !prev);
+   }
+
+   const servicesDropdownListItems = servicePageLinks.map((link, i) => (
+      <li key={`service-dropdown-link-${i}`} >
+         <PrismicNextLink field={link} className={moduleStyles.serviceLink} />
+      </li>
+   ))
+
+   return (
+      <>
+         <button
+            type="button"
+            onClick={handleServicesToggle}
+            className={clsx(
+               moduleStyles.mainServiceButton,
+               servicesOpen && moduleStyles.mainServiceButtonOpenState,
+               !servicesOpen && moduleStyles.mainServiceButtonClosedState
+            )}
+         >
+            {text}
+            <span
+               className={clsx(
+                  moduleStyles.menuArrow,
+                  moduleStyles.serviceMenuArrow,
+                  servicesOpen && moduleStyles.menuArrowDown,
+                  !servicesOpen && moduleStyles.menuArrowUp
+               )}
+            />
+         </button>
+         <div
+            className={clsx(
+               moduleStyles.servicesContainer,
+               servicesOpen && moduleStyles.servicesDisplayed,
+               !servicesOpen && moduleStyles.servicesHidden
+            )}
+         >
+            <ul className={moduleStyles.servicesList}>
+               {servicesDropdownListItems}
+            </ul>
+         </div>
+      </>
+   );
+}
