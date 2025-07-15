96943f0a428e434e038d4f13385bebcba793dae9
Dalton Pettus <daltpettus@gmail.com>
2025-07-15 15:08:49 -0700

    finish converting tailwind in Footer to module CSS

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -11,12 +11,12 @@ export default async function Footer() {
 
    return (
       <Bounded as='footer' className={moduleStyles.container}>
-         <div className="flex gap-4 items-center justify-between flex-col md:flex-row">
-            <small className="basis-1/3">Graced and Golden © {new Date().getFullYear()}</small>
-            <Link href='/' className="basis-1/3 flex justify-center">
-               <Image src='/logo-full-header.png' width={100} height={54} alt='Graced and Golden logo'className=""/>
+         <div className={moduleStyles.rowContainer}>
+            <small>Graced and Golden © {new Date().getFullYear()}</small>
+            <Link href='/' >
+               <Image src='/logo-full-header.png' width={100} height={54} alt='Graced and Golden logo' />
             </Link>
-            <ul className="basis-1/3 flex flex-col sm:flex-row gap-4 place-items-center justify-end">
+            <ul className={moduleStyles.linksContainer}>
                {footer.data.nav_link.map((link) => (
                   <li key={link.key}>
                      <PrismicNextLink key={link.key} field={link} />
