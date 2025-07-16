6b5d0fbefe5e2161d36e73caaf9ec7b628e9ae3f
Dalton Pettus <daltpettus@gmail.com>
2025-07-16 12:38:40 -0700

    add animations to HomepageHero

--- src/slices/HomepageHero/index.tsx
+++ src/slices/HomepageHero/index.tsx
@@ -1,19 +1,22 @@
-import { FC } from "react";
+'use client';
+
+import { FC, useRef } from "react";
 import { Content } from "@prismicio/client";
 import { JSXMapSerializer, PrismicRichText, SliceComponentProps } from "@prismicio/react";
 import Bounded from "@/components/Bounded";
 import Heading from "@/components/Heading";
 import Button from "@/components/Button";
 import moduleStyles from '@/slices/HomepageHero/styles.module.css';
+import useAddAnimation from "@/utilities/addAnimation";
 
 const components: JSXMapSerializer = {
   heading1: ({children}) => (
-    <Heading as="h1" size="lg" className="text-center">
+    <Heading as="h1" size="lg" className="text-center animated-element">
       {children}
     </Heading>
   ),
   paragraph: ({ children }) => (
-    <p className={`${moduleStyles.text} text-center text-3xl mr-4 mb-4`}>
+    <p className={`${moduleStyles.text} text-center text-3xl mr-4 mb-4 animated-element`}>
       {children}
     </p>
   )
@@ -28,6 +31,9 @@ export type HomepageHeroProps = SliceComponentProps<Content.HomepageHeroSlice>;
  * Component for "HomepageHero" Slices.
  */
 const HomepageHero: FC<HomepageHeroProps> = ({ slice }) => {
+  const containerRef = useRef<HTMLDivElement>(null);
+  useAddAnimation(containerRef, .5);
+  
   const getButtonColor = (index: number) => {
     switch (index) {
       case 0:
@@ -43,18 +49,20 @@ const HomepageHero: FC<HomepageHeroProps> = ({ slice }) => {
 
   return (
     <Bounded data-slice-type={slice.slice_type} data-slice-variation={slice.variation} verticalPadding={false} className={`${moduleStyles.heroContainer}`}>
-      <div className={`${moduleStyles.row} ${moduleStyles.empty}`}></div>
-      <div className={`${moduleStyles.row} ${moduleStyles.headingRow}`}>
-        <PrismicRichText field={slice.primary.main_heading} components={components}/>
+      <div ref={containerRef}>
+        <div className={`${moduleStyles.row} ${moduleStyles.empty}`}></div>
+        <div className={`${moduleStyles.row} ${moduleStyles.headingRow} animated-element`}>
+          <PrismicRichText field={slice.primary.main_heading} components={components}/>
+        </div>
+        <section className={`${moduleStyles.row} ${moduleStyles.linksRow} mt-4`}>
+          <PrismicRichText field={slice.primary.short_text} components={components}/>
+          <ul className={`${moduleStyles.links} animated-element`}>
+            {slice.primary.link.map((link, i) => (
+              <li key={link.key}><Button field={link} color={getButtonColor(i)} className={`${moduleStyles.button}`} /></li>
+            ))}
+          </ul>
+        </section>
       </div>
-      <section className={`${moduleStyles.row} ${moduleStyles.linksRow} mt-4`}>
-        <PrismicRichText field={slice.primary.short_text} components={components}/>
-        <ul className={`${moduleStyles.links}`}>
-          {slice.primary.link.map((link, i) => (
-            <li key={link.key}><Button field={link} color={getButtonColor(i)} className={`${moduleStyles.button}`} /></li>
-          ))}
-        </ul>
-      </section>
     </Bounded>
   );
 };
