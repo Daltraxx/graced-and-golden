f2ec52bae2b0d363a8ba9c4039d0c7f303a23a87
Dalton Pettus <daltpettus@gmail.com>
2025-07-20 09:00:13 -0700

    style hero for up to large mobile devices

--- src/slices/ServiceHero/index.tsx
+++ src/slices/ServiceHero/index.tsx
@@ -3,6 +3,8 @@ import { Content } from "@prismicio/client";
 import { JSXMapSerializer, PrismicRichText, SliceComponentProps } from "@prismicio/react";
 import Heading from "@/components/Heading";
 import Bounded from "@/components/Bounded";
+import moduleStyles from '@/slices/ServiceHero/styles.module.css';
+import clsx from "clsx";
 
 const components: JSXMapSerializer = {
   heading1: ({ children }) => (
@@ -11,7 +13,7 @@ const components: JSXMapSerializer = {
     </Heading>
   ),
   heading2: ({ children }) => (
-    <Heading as="h2" size="md" className='' >
+    <Heading as="h2" size="sm" className='' fontDisplay={false} >
       {children}
     </Heading>
   )
@@ -30,12 +32,18 @@ const ServiceHero: FC<ServiceHeroProps> = ({ slice }) => {
     <Bounded
       data-slice-type={slice.slice_type}
       data-slice-variation={slice.variation}
+      className={moduleStyles.boundedContainer}
     >
-      <div>
-        <section style={{ backgroundImage: `url(${slice.primary.background_image.url})`}}>
+      <div className={moduleStyles.headingsContainer} >
+        <section
+          style={{
+            backgroundImage: `url(${slice.primary.background_image.url})`,
+          }}
+          className={clsx(moduleStyles.headingSection, moduleStyles.mainheadingSection)}
+        >
           <PrismicRichText field={slice.primary.main_heading} components={components} />
         </section>
-        <section>
+        <section className={clsx(moduleStyles.headingSection, moduleStyles.subHeadingSection)} >
           <PrismicRichText field={slice.primary.sub_heading} components={components} />
         </section>
       </div>
