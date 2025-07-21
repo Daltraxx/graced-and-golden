413e97c381dbc0932090d5f55e19c1f430ac8150
Dalton Pettus <daltpettus@gmail.com>
2025-07-21 11:36:19 -0700

    some modifications to existing styles

--- src/slices/TrainingBody/index.tsx
+++ src/slices/TrainingBody/index.tsx
@@ -15,7 +15,7 @@ const components: JSXMapSerializer = {
     </Heading>
   ),
   heading3: ({ children }) => (
-    <Heading as="h3" size="md" className='' >
+    <Heading as="h3" size="sm" className='' >
       {children}
     </Heading>
   ),
