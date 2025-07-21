ed43061bba4c2ab988b34e62a9ddaf912b185ed6
Dalton Pettus <daltpettus@gmail.com>
2025-07-20 18:04:51 -0700

    finish rich text serializer and format

--- src/slices/TrainingBody/index.tsx
+++ src/slices/TrainingBody/index.tsx
@@ -4,6 +4,7 @@ import { JSXMapSerializer, PrismicRichText, SliceComponentProps } from "@prismic
 import Heading from "@/components/Heading";
 import Bounded from "@/components/Bounded";
 import { PrismicNextImage, PrismicNextLink } from "@prismicio/next";
+import moduleStyles from '@/slices/TrainingBody/styles.module.css';
 
 const components: JSXMapSerializer = {
   heading2: ({ children }) => (
@@ -21,6 +22,14 @@ const components: JSXMapSerializer = {
       {children}
     </Heading>
   ),
+  paragraph: ({ children }) => (
+    <p>{children}</p>
+  ),
+  list: ({ children }) => (
+    <ul className="list-disc">
+      {children}
+    </ul>
+  )
 }
 
 /**
@@ -36,56 +45,43 @@ const TrainingBody: FC<TrainingBodyProps> = ({ slice }) => {
     <Bounded
       data-slice-type={slice.slice_type}
       data-slice-variation={slice.variation}
+      className={moduleStyles.boundedContainer}
     >
       <PrismicRichText field={slice.primary.main_heading} />
       <section>
         <div>
-          <PrismicRichText field={slice.primary.intro_paragraph_1} />
-          <PrismicRichText field={slice.primary.intro_paragraph_2} />
+          <PrismicRichText field={slice.primary.intro_paragraph_1} components={components} />
+          <PrismicRichText field={slice.primary.intro_paragraph_2} components={components} />
         </div>
         <PrismicNextImage field={slice.primary.image} />
       </section>
       <section>
         <section>
-          <PrismicRichText field={slice.primary.includes_heading} />
-          <PrismicRichText field={slice.primary.includes_list} />
+          <PrismicRichText field={slice.primary.includes_heading} components={components} />
+          <PrismicRichText field={slice.primary.includes_list} components={components} />
         </section>
         <section>
-          <PrismicRichText
-            field={slice.primary.includes_sidebar_heading}
-          />
-          <PrismicRichText
-            field={slice.primary.includes_sidebar_body}
-          />
+          <PrismicRichText field={slice.primary.includes_sidebar_heading} components={components} />
+          <PrismicRichText field={slice.primary.includes_sidebar_body} components={components} />
         </section>
       </section>
       <div>
         <section>
-          <PrismicRichText
-            field={slice.primary.requirements_heading}
-          />
-          <PrismicRichText field={slice.primary.requirements_body} />
+          <PrismicRichText field={slice.primary.requirements_heading} components={components} />
+          <PrismicRichText field={slice.primary.requirements_body} components={components} />
         </section>
         <section>
-          <PrismicRichText
-            field={slice.primary.format_and_location_heading}
-          />
-          <PrismicRichText
-            field={slice.primary.format_and_location_details_list}
-          />
+          <PrismicRichText field={slice.primary.format_and_location_heading} components={components} />
+          <PrismicRichText field={slice.primary.format_and_location_details_list} components={components} />
         </section>
         <section>
-          <PrismicRichText
-            field={slice.primary.booking_and_availability_heading}
-          />
-          <PrismicRichText
-            field={slice.primary.booking_and_availability_body}
-          />
+          <PrismicRichText field={slice.primary.booking_and_availability_heading} components={components} />
+          <PrismicRichText field={slice.primary.booking_and_availability_body} components={components} />
         </section>
       </div>
       <section>
-        <PrismicRichText field={slice.primary.link_box_heading} />
-        <PrismicRichText field={slice.primary.link_box_body} />
+        <PrismicRichText field={slice.primary.link_box_heading} components={components} />
+        <PrismicRichText field={slice.primary.link_box_body} components={components} />
         {slice.primary.link.map((link) => (
           <PrismicNextLink
             key={link.key}
