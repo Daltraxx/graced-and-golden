2d878338ef0dd2c4c612314d2d07f32cc62954db
Dalton Pettus <daltpettus@gmail.com>
2025-08-02 08:46:22 -0700

    get both arrows for cycler working, clearing interval and removing fade-in-out classes to be replaced with only fade in for selected next testimonial

--- src/components/TestimonialCycler/TestimonialCycler.tsx
+++ src/components/TestimonialCycler/TestimonialCycler.tsx
@@ -1,50 +1,56 @@
-'use client';
+"use client";
 
 import { JSX, useEffect, useRef, useState } from "react";
-import moduleStyles from '@/components/TestimonialCycler/styles.module.css';
+import moduleStyles from "@/components/TestimonialCycler/styles.module.css";
 import clsx from "clsx";
 import ArrowIcon from "../ArrowIcon";
 
-export default function TestimonialCycler({ testimonials }: { testimonials: JSX.Element[] }) {
+export default function TestimonialCycler({
+  testimonials,
+}: {
+  testimonials: JSX.Element[];
+}) {
   const [testimonial, setTestimonial] = useState({
     text: testimonials[0],
-    index: 0
+    index: 0,
   });
 
   const [cyclerActive, setCyclerActive] = useState(true);
 
   type TestimonialState = {
-    text: JSX.Element,
-    index: number
-  }
+    text: JSX.Element;
+    index: number;
+  };
 
-  const setNextTestimonial = (prevTestimonialState: TestimonialState) => {
+  const getNextTestimonialState = (prevTestimonialState: TestimonialState) => {
     const nextIndex = (prevTestimonialState.index + 1) % testimonials.length;
     return {
       text: testimonials[nextIndex],
-      index: nextIndex
+      index: nextIndex,
     };
-  }
+  };
+
+  const getPrevTestimonialState = (prevTestimonialState: TestimonialState) => {
+    const n = testimonials.length;
+    const nextIndex = (prevTestimonialState.index - 1 + n) % n;
+    return {
+      text: testimonials[nextIndex],
+      index: nextIndex,
+    };
+  };
 
   let interval: NodeJS.Timeout;
   useEffect(() => {
-    interval = setInterval(() => {
-      setTestimonial(prev => {
-        return setNextTestimonial(prev);
-      });
-    }, 10000);
+    if (cyclerActive) {
+      interval = setInterval(() => {
+        setTestimonial((prev) => {
+          return getNextTestimonialState(prev);
+        });
+      }, 10000);
+    }
 
     return () => clearInterval(interval);
   }, [testimonials]);
-
-  const handleArrowClick = () => {
-    if (cyclerActive) {
-      setCyclerActive(false);
-      clearInterval(interval);
-    }
-
-    setTestimonial(prev => setNextTestimonial(prev));
-  }
 
   const testimonialRef = useRef<HTMLQuoteElement>(null);
 
@@ -63,30 +69,60 @@ export default function TestimonialCycler({ testimonials }: { testimonials: JSX.
       }
     } else {
       if (testimonialContainer) {
-        if (testimonialContainer.classList.contains(moduleStyles.animationPermanent1)) {
-          testimonialContainer.classList.remove(moduleStyles.animationPermanent1);
+        if (
+          testimonialContainer.classList.contains(
+            moduleStyles.animationPermanent1
+          )
+        ) {
+          testimonialContainer.classList.remove(
+            moduleStyles.animationPermanent1
+          );
           testimonialContainer.classList.add(moduleStyles.animationPermanent2);
         } else {
-          testimonialContainer.classList.remove(moduleStyles.animationPermanent2);
+          testimonialContainer.classList.remove(
+            moduleStyles.animationPermanent2
+          );
           testimonialContainer.classList.add(moduleStyles.animationPermanent1);
         }
       }
     }
-  }, [testimonial])
+  }, [testimonial]);
+
+  const handleArrowClick = (direction: "left" | "right") => {
+    if (cyclerActive) {
+      setCyclerActive(false);
+      clearInterval(interval);
+      if (testimonialRef.current) {
+        testimonialRef.current.classList.remove(moduleStyles.animation1);
+        testimonialRef.current.classList.remove(moduleStyles.animation2);
+      }
+    }
+
+    direction === "left"
+      ? setTestimonial((prev) => getPrevTestimonialState(prev))
+      : setTestimonial((prev) => getNextTestimonialState(prev));
+  };
 
   return (
-    <div className={moduleStyles.sectionContainer} >
-      <blockquote ref={testimonialRef} className={moduleStyles.quoteContainer} >
+    <div className={moduleStyles.sectionContainer}>
+      <blockquote ref={testimonialRef} className={moduleStyles.quoteContainer}>
         {testimonial.text}
       </blockquote>
-      <div className={moduleStyles.buttonsContainer} >
-        <button>
-          <ArrowIcon className={clsx(moduleStyles.arrowIcon, moduleStyles.arrowIconLeft)} />
+      <div className={moduleStyles.buttonsContainer}>
+        <button onClick={() => handleArrowClick("left")}>
+          <ArrowIcon
+            className={clsx(moduleStyles.arrowIcon, moduleStyles.arrowIconLeft)}
+          />
         </button>
-        <button onClick={handleArrowClick}>
-          <ArrowIcon className={clsx(moduleStyles.arrowIcon, moduleStyles.arrowIconRight)} />
+        <button onClick={() => handleArrowClick("right")}>
+          <ArrowIcon
+            className={clsx(
+              moduleStyles.arrowIcon,
+              moduleStyles.arrowIconRight
+            )}
+          />
         </button>
-       </div>
+      </div>
     </div>
   );
 }
