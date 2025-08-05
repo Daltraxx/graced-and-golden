2d275b6e150bc35e6e4e70a37edddcdd29d69101
Dalton Pettus <daltpettus@gmail.com>
2025-08-04 19:36:57 -0700

    add animations to ParagraphImageOverlap

--- src/slices/ParagraphImageOverlap/index.tsx
+++ src/slices/ParagraphImageOverlap/index.tsx
@@ -1,9 +1,13 @@
-import { FC } from "react";
+'use client';
+
+import { FC, useRef } from "react";
 import { Content } from "@prismicio/client";
 import { JSXMapSerializer, PrismicRichText, SliceComponentProps } from "@prismicio/react";
 import Heading from "@/components/Heading";
 import Bounded from "@/components/Bounded";
 import moduleStyles from '@/slices/ParagraphImageOverlap/styles.module.css';
+import useAddAnimation from "@/utilities/addAnimation";
+import clsx from "clsx";
 
 const components: JSXMapSerializer = {
   heading3: ({ children }) => (
@@ -26,6 +30,9 @@ export type ParagraphImageOverlapProps =
  * Component for "ParagraphImageOverlap" Slices.
  */
 const ParagraphImageOverlap: FC<ParagraphImageOverlapProps> = ({ slice }) => {
+  const containerRef = useRef<HTMLElement>(null);
+  useAddAnimation(containerRef, .3);
+  
   return (
     <Bounded
       data-slice-type={slice.slice_type}
@@ -33,13 +40,25 @@ const ParagraphImageOverlap: FC<ParagraphImageOverlapProps> = ({ slice }) => {
       className={moduleStyles.boundedContainer}
       horizontalSpacing={false}
       verticalPadding={false}
+      ref={containerRef}
     >
-      <div className={moduleStyles.contentContainer} >
-        <section className={moduleStyles.textContainer} >
-          <PrismicRichText field={slice.primary.heading} components={components} />
-          <PrismicRichText field={slice.primary.body_text} components={components} />
+      <div className={clsx(moduleStyles.contentContainer)}>
+        <section className={(moduleStyles.textContainer)}>
+          <div className={clsx(moduleStyles.textDiv, "animated-element")} >
+            <PrismicRichText
+              field={slice.primary.heading}
+              components={components}
+            />
+            <PrismicRichText
+              field={slice.primary.body_text}
+              components={components}
+            />
+          </div>
         </section>
-        <div style={{ backgroundImage: `url(${slice.primary.image.url})`}} className={moduleStyles.imageContainer} ></div>
+        <div
+          style={{ backgroundImage: `url(${slice.primary.image.url})` }}
+          className={moduleStyles.imageContainer}
+        ></div>
       </div>
     </Bounded>
   );
