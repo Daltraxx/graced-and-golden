53fd574998832ebdf5fa890eaad0ef7f2303a1bc
Dalton Pettus <daltpettus@gmail.com>
2025-08-04 18:47:07 -0700

    add animations to Bridal Package

--- src/slices/BridalPackage/index.tsx
+++ src/slices/BridalPackage/index.tsx
@@ -1,10 +1,14 @@
-import { FC } from "react";
+'use client';
+
+import { FC, useRef } from "react";
 import { Content } from "@prismicio/client";
 import { JSXMapSerializer, PrismicRichText, SliceComponentProps } from "@prismicio/react";
 import Heading from "@/components/Heading";
 import moduleStyles from '@/slices/BridalPackage/styles.module.css';
 import Bounded from "@/components/Bounded";
 import BridalPackageCard from "@/components/BridalPackageCard/BridalPackageCard";
+import useAddAnimation from "@/utilities/addAnimation";
+import clsx from "clsx";
 
 const components: JSXMapSerializer = {
   heading2: ({children}) => (
@@ -27,20 +31,42 @@ export type BridalPackageProps =
  * Component for "BridalPackage" Slices.
  */
 const BridalPackage: FC<BridalPackageProps> = ({ slice }) => {
+  const containerRef = useRef<HTMLElement>(null);
+  useAddAnimation(containerRef);
+  
   return (
     <Bounded
       data-slice-type={slice.slice_type}
       data-slice-variation={slice.variation}
       className={moduleStyles.boundedContainer}
+      ref={containerRef}
     >
       <div className={moduleStyles.contentContainer}>
-        <section className={`${moduleStyles.cardContainer} ${moduleStyles.explainerContainer}`}>
-          <PrismicRichText field={slice.primary.package_name} components={components} />
-          <div className={moduleStyles.explainerTextContainer} >
-            <PrismicRichText field={slice.primary.package_body_text} components={components} />
+        <section
+          className={clsx(
+            moduleStyles.cardContainer,
+            moduleStyles.explainerContainer,
+            "animated-element"
+          )}
+        >
+          <PrismicRichText
+            field={slice.primary.package_name}
+            components={components}
+          />
+          <div className={moduleStyles.explainerTextContainer}>
+            <PrismicRichText
+              field={slice.primary.package_body_text}
+              components={components}
+            />
           </div>
         </section>
-        <BridalPackageCard slice={slice} className={moduleStyles.cardContainer} index={0} slices={[]} context={undefined} />
+        <BridalPackageCard
+          slice={slice}
+          className={clsx(moduleStyles.cardContainer, "animated-element")}
+          index={0}
+          slices={[]}
+          context={undefined}
+        />
       </div>
     </Bounded>
   );
