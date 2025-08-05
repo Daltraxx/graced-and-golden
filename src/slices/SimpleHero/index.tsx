4c3f17e761e4405ba3bd1ed732871e03cff8f21a
Dalton Pettus <daltpettus@gmail.com>
2025-08-04 19:12:44 -0700

    add animations to SimpleHero

--- src/slices/SimpleHero/index.tsx
+++ src/slices/SimpleHero/index.tsx
@@ -1,9 +1,13 @@
-import { FC } from "react";
+'use client';
+
+import { FC, useRef } from "react";
 import { Content } from "@prismicio/client";
 import { JSXMapSerializer, PrismicRichText, SliceComponentProps } from "@prismicio/react";
 import Heading from "@/components/Heading";
 import Bounded from "@/components/Bounded";
 import moduleStyles from '@/slices/SimpleHero/styles.module.css';
+import useAddAnimation from "@/utilities/addAnimation";
+import clsx from "clsx";
 
 const components: JSXMapSerializer = {
   heading1: ({ children }) => (
@@ -27,17 +31,33 @@ export type SimpleHeroProps = SliceComponentProps<Content.SimpleHeroSlice>;
  * Component for "SimpleHero" Slices.
  */
 const SimpleHero: FC<SimpleHeroProps> = ({ slice }) => {
+  const containerRef = useRef<HTMLElement>(null);
+  useAddAnimation(containerRef);
+
   const backgroundImageURL =
     slice.primary.background_image.url || "/bridal-mags-reduce-dark-min.webp";
+  
   return (
     <Bounded
       data-slice-type={slice.slice_type}
       data-slice-variation={slice.variation}
       className={moduleStyles.boundedContainer}
+      ref={containerRef}
     >
-      <div style={{backgroundImage: `url(${backgroundImageURL})`}} className={moduleStyles.heroContainer} >
-        <PrismicRichText field={slice.primary.main_heading} components={components} />
-        <PrismicRichText field={slice.primary.sub_heading} components={components} />
+      <div
+        style={{ backgroundImage: `url(${backgroundImageURL})` }}
+        className={moduleStyles.heroContainer}
+      >
+        <div className={clsx(moduleStyles.textContainer, "animated-element")}>
+          <PrismicRichText
+            field={slice.primary.main_heading}
+            components={components}
+          />
+          <PrismicRichText
+            field={slice.primary.sub_heading}
+            components={components}
+          />
+        </div>
       </div>
     </Bounded>
   );
