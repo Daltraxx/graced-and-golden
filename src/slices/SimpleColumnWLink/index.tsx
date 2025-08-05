8ad8df0b9c1a71d3d74d5ef50bbca2d8f4aa9afc
Dalton Pettus <daltpettus@gmail.com>
2025-08-05 08:16:25 -0700

    add animations to SimpleColumnWLink

--- src/slices/SimpleColumnWLink/index.tsx
+++ src/slices/SimpleColumnWLink/index.tsx
@@ -1,10 +1,14 @@
-import { FC } from "react";
+'use client';
+
+import { FC, useRef } from "react";
 import { Content } from "@prismicio/client";
 import { JSXMapSerializer, PrismicRichText, SliceComponentProps } from "@prismicio/react";
 import Heading from "@/components/Heading";
 import Bounded from "@/components/Bounded";
 import Button from "@/components/Button/Button";
 import moduleStyles from '@/slices/SimpleColumnWLink/styles.module.css';
+import useAddAnimation from "@/utilities/addAnimation";
+import clsx from "clsx";
 
 const components: JSXMapSerializer = {
   heading2: ({ children }) => (
@@ -32,13 +36,17 @@ export type SimpleColumnWLinkProps =
  * Component for "SimpleColumnWLink" Slices.
  */
 const SimpleColumnWLink: FC<SimpleColumnWLinkProps> = ({ slice }) => {
+  const containerRef = useRef<HTMLElement>(null);
+  useAddAnimation(containerRef);
+
   return (
     <Bounded
       data-slice-type={slice.slice_type}
       data-slice-variation={slice.variation}
       className={moduleStyles.boundedContainer}
+      ref={containerRef}
     >
-      <div className={moduleStyles.contentContainer} >
+      <div className={clsx(moduleStyles.contentContainer, 'animated-element')} >
         <PrismicRichText field={slice.primary.heading} components={components} />
         <PrismicRichText field={slice.primary.body_text} components={components} />
         <Button field={slice.primary.link} color="brown-500" />
