1e4bc96aadae89905d19af5ceec8352b62d86a61
Dalton Pettus <daltpettus@gmail.com>
2025-08-05 08:12:45 -0700

    add animations to About

--- src/slices/About/index.tsx
+++ src/slices/About/index.tsx
@@ -1,4 +1,6 @@
-import { FC } from "react";
+'use client';
+
+import { FC, useEffect, useRef } from "react";
 import { Content } from "@prismicio/client";
 import { JSXMapSerializer, PrismicRichText, SliceComponentProps } from "@prismicio/react";
 import Heading from "@/components/Heading";
@@ -6,6 +8,8 @@ import Bounded from "@/components/Bounded";
 import Button from "@/components/Button/Button";
 import { PrismicNextImage } from "@prismicio/next";
 import moduleStyles from '@/slices/About/styles.module.css';
+import useAddAnimation from "@/utilities/addAnimation";
+import clsx from "clsx";
 
 const components: JSXMapSerializer = {
   heading1: ({ children }) => (
@@ -37,28 +41,35 @@ export type AboutProps = SliceComponentProps<Content.AboutSlice>;
  * Component for "About" Slices.
  */
 const About: FC<AboutProps> = ({ slice }) => {
+  const containerRef = useRef<HTMLElement>(null);
+  useAddAnimation(containerRef, 0.3);
+  
   return (
     <Bounded
       data-slice-type={slice.slice_type}
       data-slice-variation={slice.variation}
       horizontalSpacing={false}
       className={moduleStyles.boundedContainer}
+      ref={containerRef}
     >
-      <PrismicRichText field={slice.primary.main_heading} components={components} />
+      <div className="animated-element" >
+        <PrismicRichText field={slice.primary.main_heading} components={components} />
+      </div>
       <div className={moduleStyles.contentContainer} >
-        <section className={moduleStyles.textContainer} >
-          <PrismicRichText field={slice.primary.text_heading} components={components} />
+        <section className={clsx(moduleStyles.textContainer)} >
+          <div className="animated-element" >
+            <PrismicRichText field={slice.primary.text_heading} components={components} /></div>
           <div className={moduleStyles.paragraphsContainer}>
-            <div className={moduleStyles.paragraphColumn} >
+            <div className={clsx(moduleStyles.paragraphColumn, 'animated-element')}>
               <PrismicRichText field={slice.primary.text_column_1} components={components} />
             </div>
-            <div className={moduleStyles.paragraphColumn} >
+            <div className={clsx(moduleStyles.paragraphColumn, 'animated-element')} >
               <PrismicRichText field={slice.primary.text_column_2} components={components} />
               <Button field={slice.primary.link} color="cream-200" className={moduleStyles.button} />
             </div>
           </div>
         </section>
-        <div className={moduleStyles.imageContainer} >
+        <div className={clsx(moduleStyles.imageContainer, 'animated-element')} >
           <PrismicNextImage field={slice.primary.front_image} className={moduleStyles.frontImage} />
           <PrismicNextImage field={slice.primary.back_image} className={moduleStyles.backImage} />
         </div>
