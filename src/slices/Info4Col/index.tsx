2d055dfc255f8193c9093a49c0832f90a2d7d8e0
Dalton Pettus <daltpettus@gmail.com>
2025-08-04 18:40:51 -0700

    add animations to Info4Col

--- src/slices/Info4Col/index.tsx
+++ src/slices/Info4Col/index.tsx
@@ -1,9 +1,13 @@
-import { FC } from "react";
+'use client';
+
+import { FC, useRef } from "react";
 import { Content } from "@prismicio/client";
 import { JSXMapSerializer, PrismicRichText, SliceComponentProps } from "@prismicio/react";
 import Heading from "@/components/Heading";
 import Bounded from "@/components/Bounded";
 import moduleStyles from '@/slices/Info4Col/styles.module.css';
+import useAddAnimation from "@/utilities/addAnimation";
+import clsx from "clsx";
 
 const components: JSXMapSerializer = {
   heading2: ({children}) => (
@@ -30,17 +34,29 @@ export type Info4ColProps = SliceComponentProps<Content.Info4ColSlice>;
  * Component for "Info4Col" Slices.
  */
 const Info4Col: FC<Info4ColProps> = ({ slice }) => {
+  const containerRef = useRef<HTMLElement>(null);
+  useAddAnimation(containerRef);
+  
   return (
     <Bounded
       data-slice-type={slice.slice_type}
       data-slice-variation={slice.variation}
       className={moduleStyles.boundedContainer}
       horizontalSpacing={false}
+      ref={containerRef}
     >
-      <PrismicRichText field={slice.primary.main_heading} components={components} />
+      <div className="animated-element">
+        <PrismicRichText
+          field={slice.primary.main_heading}
+          components={components}
+        />
+      </div>
       <div className={moduleStyles.boxesContainer}>
         {slice.primary.column.map((item, i) => (
-          <section className={moduleStyles.box} key={`info-col-${i}`}>
+          <section
+            className={clsx(moduleStyles.box, "animated-element")}
+            key={`info-col-${i}`}
+          >
             <PrismicRichText field={item.heading} components={components} />
             <PrismicRichText field={item.body_text} components={components} />
           </section>
