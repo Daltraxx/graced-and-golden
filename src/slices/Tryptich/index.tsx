e419c2a21cdafdcecf4318b07d66485dd0505f9c
Dalton Pettus <daltpettus@gmail.com>
2025-08-06 11:56:32 -0700

    use clsx instead of interpolation

--- src/slices/Tryptich/index.tsx
+++ src/slices/Tryptich/index.tsx
@@ -8,6 +8,7 @@ import { PrismicNextImage, PrismicNextLink } from "@prismicio/next";
 import moduleStyles from '@/slices/Tryptich/styles.module.css';
 import Heading from "@/components/Heading";
 import useAddAnimation from "@/utilities/addAnimation";
+import clsx from "clsx";
 
 const components: JSXMapSerializer = {
   heading2: ({ children }) => (
@@ -34,19 +35,52 @@ const Tryptich: FC<TryptichProps> = ({ slice }) => {
   useAddAnimation(containerRef, .5);
 
   return (
-    <Bounded data-slice-type={slice.slice_type} data-slice-variation={slice.variation} className={`${moduleStyles.triptychContainer}`}>
+    <Bounded
+      data-slice-type={slice.slice_type}
+      data-slice-variation={slice.variation}
+      className={`${moduleStyles.triptychContainer}`}
+    >
       <div ref={containerRef} className={`${moduleStyles.row}`}>
-        <div style={{ backgroundImage: `url(${slice.primary.image_left.url})`}} className={`${moduleStyles.bgImageContainer} animated-element`}></div>
+        <div
+          style={{ backgroundImage: `url(${slice.primary.image_left.url})` }}
+          className={clsx(moduleStyles.bgImageContainer, "animated-element")}
+        ></div>
         <div className={`${moduleStyles.bodyContainer}`}>
           <section className={`${moduleStyles.bodyText}`}>
             <p className="mb-2 animated-element">{slice.primary.small_text}</p>
-            <div className='animated-element'><PrismicRichText field={slice.primary.heading} components={components} /></div>
-            <div className='animated-element'><PrismicRichText field={slice.primary.body} components={components} /></div>
-            <div className='animated-element'><PrismicNextLink field={slice.primary.link} className={`${moduleStyles.bodyTextLink}`}/></div>
-            <PrismicNextImage field={slice.primary.body_image} width={610} height={380} className='animated-element'/>
+            <div className="animated-element">
+              <PrismicRichText
+                field={slice.primary.heading}
+                components={components}
+              />
+            </div>
+            <div className="animated-element">
+              <PrismicRichText
+                field={slice.primary.body}
+                components={components}
+              />
+            </div>
+            <div className="animated-element">
+              <PrismicNextLink
+                field={slice.primary.link}
+                className={moduleStyles.bodyTextLink}
+              />
+            </div>
+            <PrismicNextImage
+              field={slice.primary.body_image}
+              width={610}
+              height={380}
+              className="animated-element"
+            />
           </section>
         </div>
-        <div style={{ backgroundImage: `url(${slice.primary.image_right.url})`}} className={`${moduleStyles.bgImageContainer} animated-element`}></div>
+        <div
+          style={{ backgroundImage: `url(${slice.primary.image_right.url})` }}
+          className={clsx(
+            moduleStyles.bgImageContainer,
+            "animated-element"
+          )}
+        ></div>
       </div>
     </Bounded>
   );
