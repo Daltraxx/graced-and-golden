265e790c2b4d884ae836a7062c45ec0ad786f09f
Dalton Pettus <daltpettus@gmail.com>
2025-08-13 20:03:58 -0700

    prepare refs and usestate for use in button toggle to display section texts

--- src/slices/InformationPanel/index.tsx
+++ src/slices/InformationPanel/index.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { FC, useRef } from "react";
+import { FC, useRef, useState } from "react";
 import { Content } from "@prismicio/client";
 import { JSXMapSerializer, PrismicRichText, SliceComponentProps } from "@prismicio/react";
 import Heading from "@/components/Heading";
@@ -8,6 +8,8 @@ import moduleStyles from '@/slices/InformationPanel/styles.module.css';
 import Bounded from "@/components/Bounded";
 import useAddAnimation from "@/utilities/addAnimation";
 import clsx from "clsx";
+import MenuToggleButton from "@/components/MenuToggleButton/MenuToggleButton";
+import { info } from "console";
 
 const components: JSXMapSerializer = {
   heading3: ({ children }) => (
@@ -37,6 +39,24 @@ export type InformationPanelProps =
 const InformationPanel: FC<InformationPanelProps> = ({ slice }) => {
   const containerRef = useRef<HTMLElement>(null);
   useAddAnimation(containerRef, 0.1);
+
+  const getInfoOpenStateMap = (numberOfSections: number) => {
+    const infoOpenStateMap = new Map();
+    for (let i = 0; i < numberOfSections; i++) {
+      infoOpenStateMap.set(i, false);
+    }
+
+    return infoOpenStateMap;
+  }
+
+  const [infoOpen, setInfoOpen] = useState(
+    getInfoOpenStateMap(slice.primary.info_block.length)
+  );
+
+  
+  const buttonRefs = useRef<(HTMLButtonElement | null)[]>(
+    Array(slice.primary.info_block.length).fill(null)
+  );
   
   return (
     <Bounded
@@ -57,11 +77,20 @@ const InformationPanel: FC<InformationPanelProps> = ({ slice }) => {
             key={`info-section-${i}`}
             className={clsx(moduleStyles.sectionContainer, "animated-element")}
           >
-            <PrismicRichText
-              field={item.info_heading}
-              components={components}
-            />
-            <PrismicRichText field={item.info_body} components={components} />
+            <Heading as="h4" size="xs" className={moduleStyles.sectionHeading}>
+              {item.info_heading}
+              {/* <MenuToggleButton
+                displayText={`${item.info_heading}`}
+                ariaControlsId={`Info Details ${i}`}
+                buttonToggleRef={(el: HTMLButtonElement) => {
+                  if (buttonRefs.current) buttonRefs.current[i] = el;
+                }}
+
+              /> */}
+            </Heading>
+            <div id={`Info Details ${i}`}>
+              <PrismicRichText field={item.info_body} components={components} />
+            </div>
           </section>
         ))}
       </section>
