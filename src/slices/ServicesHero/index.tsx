1f3ce7a817777c691abed236518c003897f57539
Dalton Pettus <daltpettus@gmail.com>
2025-08-15 10:44:55 -0700

    remove empty classnames

--- src/slices/ServicesHero/index.tsx
+++ src/slices/ServicesHero/index.tsx
@@ -12,12 +12,12 @@ import clsx from "clsx";
 
 const components: JSXMapSerializer = {
   heading1: ({children}) => (
-    <Heading as="h1" size="manual" className="">
+    <Heading as="h1" size="manual" >
       {children}
     </Heading>
   ),
   heading2: ({ children }) => (
-    <Heading as="h2" size="manual" className="">
+    <Heading as="h2" size="manual" >
       {children}
     </Heading>
   )
