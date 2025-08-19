55979d2265b8a36893fd06d87fe5338973a18683
Dalton Pettus <daltpettus@gmail.com>
2025-08-19 16:51:29 -0700

    remove empty classname

--- src/slices/InfoListWideImage/index.tsx
+++ src/slices/InfoListWideImage/index.tsx
@@ -10,12 +10,12 @@ import useAddAnimation from "@/utilities/addAnimation";
 
 const components: JSXMapSerializer = {
   heading2: ({children}) => (
-    <Heading as="h2" size="lg" className="">
+    <Heading as="h2" size="lg" >
         {children}
     </Heading>
   ),
   heading3: ({children}) => (
-    <Heading as="h3" size="sm" className="">
+    <Heading as="h3" size="sm" >
         {children}
     </Heading>
   ),
