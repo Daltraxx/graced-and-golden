dfb1114cddb69c2ddf474faa7d1449d91b565208
Dalton Pettus <daltpettus@gmail.com>
2025-08-20 08:28:19 -0700

    remove redundant applyFontSpacing prop, already removed because of cursive

--- src/slices/HeroLargeImageWithLinks/index.tsx
+++ src/slices/HeroLargeImageWithLinks/index.tsx
@@ -13,7 +13,7 @@ import clsx from "clsx";
 
 const components: JSXMapSerializer = {
   heading1: ({ children }) => (
-    <Heading as="h1" size="manual" font="cursive" applyFontSpacing={false}>
+    <Heading as="h1" size="manual" font="cursive">
       {children}
     </Heading>
   ),
