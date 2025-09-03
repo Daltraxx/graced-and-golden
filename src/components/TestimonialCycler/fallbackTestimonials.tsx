aff3c2a3035b75d128a8ac44761bbd5c7cc604bd
Dalton Pettus <daltpettus@gmail.com>
2025-09-03 15:37:05 -0700

    add keys to fallback testimonials

--- src/components/TestimonialCycler/fallbackTestimonials.tsx
+++ src/components/TestimonialCycler/fallbackTestimonials.tsx
@@ -1,20 +1,20 @@
 import { JSX } from "react";
 
 const fallbackTestimonials: JSX.Element[] = [
-  <p>
+  <p key="fallback-testimonial-1">
     "Grace is absolutely amazing. By far, the only person I trust with my spray
     tans. It looks so natural and flawless, and she gives you all as long as
     possible, comfortable and welcome and suits your needs. If you need a spray
     tan in San Diego Grace is your girl. Highly highly recommend Grace!"
   </p>,
-  <p>
+  <p key="fallback-testimonial-2">
     "I've been going to Grace for my tans for about a year now and every single
     time my tan has turned out flawless and so natural looking! She will help
     you decide the right tan for you & makes you feel so comfortable. I highly
     recommend grace to anyone looking to get a perfect, natural, long lasting
     spray tan!"
   </p>,
-  <p>
+  <p key="fallback-testimonial-3">
     "Grace is amazing! Her tan stayed on for such a long time and looked great!
     I got so many compliments saying how natural my tan looked for my bridal
     shower! She makes you feel so comfortable and I will definitely be using her
