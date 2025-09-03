33e6af90223fec3b3109babd94fbb490867c7ab1
Dalton Pettus <daltpettus@gmail.com>
2025-09-02 18:12:32 -0700

    formatting

--- src/components/Header/Nav/Nav.tsx
+++ src/components/Header/Nav/Nav.tsx
@@ -1,85 +1,91 @@
-'use client';
+"use client";
 
 import clsx from "clsx";
 import { useEffect, useRef, useState } from "react";
-import moduleStyles from '@/components/Header/Nav/styles.module.css'
+import moduleStyles from "@/components/Header/Nav/styles.module.css";
 import { PrismicNextLink } from "@prismicio/next";
 import { LinkField } from "@prismicio/client";
 import ServicesMenu from "./ServicesMenu/ServicesMenu";
 import MenuToggleButton from "@/components/MenuToggleButton/MenuToggleButton";
 
-export default function Nav({ navLinks, servicePageLinks }: { navLinks: LinkField[], servicePageLinks: LinkField[] }) {
-   const [navMenuOpen, setNavMenuOpen] = useState(false);
-   const navButtonRef = useRef<HTMLButtonElement>(null);
-   const serviceMenuRef = useRef<HTMLLIElement>(null);
+export default function Nav({
+  navLinks,
+  servicePageLinks,
+}: {
+  navLinks: LinkField[];
+  servicePageLinks: LinkField[];
+}) {
+  const [navMenuOpen, setNavMenuOpen] = useState(false);
+  const navButtonRef = useRef<HTMLButtonElement>(null);
+  const serviceMenuRef = useRef<HTMLLIElement>(null);
 
-   const setNavClosed = () => setNavMenuOpen(false);
-   
-   const toggleNavMenuOpenState = () => {
-      setNavMenuOpen(prev => !prev);
-   }
+  const setNavClosed = () => setNavMenuOpen(false);
 
-   // Have Nav Menu close (mobile) if link is clicked
-   useEffect(() => {
-      if (!navMenuOpen) return;
-      const handleLinkClick = ({ target } : MouseEvent) => {
-         const nodeTarget = target as Node;
-         if (navButtonRef.current?.contains(nodeTarget)) return;
-         if (serviceMenuRef.current?.contains(nodeTarget)) return;
-         setNavClosed();
-      }
-      window.addEventListener('mousedown', handleLinkClick);
-      return () => {
-         window.removeEventListener('mousedown', handleLinkClick);
-      };
-   }, [navMenuOpen])
+  const toggleNavMenuOpenState = () => {
+    setNavMenuOpen((prev) => !prev);
+  };
 
-   const navListItems = navLinks.map((link, i) => {
-      // consider changing comparison to be based on something else such as slug or uid
-      if (link.text && link.text.toLowerCase() === 'services') {
-         return (
-           <li key={`nav-link-${i}`} ref={serviceMenuRef}>
-             <ServicesMenu
-               linkDisplayText={link.text}
-               servicePageLinks={servicePageLinks}
-               setNavClosedAction={setNavClosed}
-               navButtonRef={navButtonRef}
-             />
-           </li>
-         );
-      } else {
-         return (
-            <li key={`nav-link-${i}`}>
-               <PrismicNextLink field={link} className={moduleStyles.navLink}/>
-            </li>
-         );
-      }
-   })
+  // Have Nav Menu close (mobile) if link is clicked
+  useEffect(() => {
+    if (!navMenuOpen) return;
+    const handleLinkClick = ({ target }: MouseEvent) => {
+      const nodeTarget = target as Node;
+      if (navButtonRef.current?.contains(nodeTarget)) return;
+      if (serviceMenuRef.current?.contains(nodeTarget)) return;
+      setNavClosed();
+    };
+    window.addEventListener("mousedown", handleLinkClick);
+    return () => {
+      window.removeEventListener("mousedown", handleLinkClick);
+    };
+  }, [navMenuOpen]);
 
-   return (
-      <nav className={moduleStyles.navContainer} aria-label="Main Navigation">
-         <MenuToggleButton
-            displayText="MENU"
-            menuOpen={navMenuOpen}
-            onToggle={toggleNavMenuOpenState}
-            buttonToggleRef={navButtonRef}
-            menuOnlyMobile={true}
-            arrowOnlyMobile={true}
-            ariaControlsId="nav-menu"
-            arrowSize="large"
-            arrowColor="brown-500"
-            className={moduleStyles.menuToggleButton}
-         />
-         <ul
-            id="nav-menu"
-            className={clsx(
-            moduleStyles.linksContainer,
-            navMenuOpen && moduleStyles.linksDisplayed,
-            !navMenuOpen && moduleStyles.linksHidden
-            )}
-         >
-            {navListItems}
-         </ul>
-      </nav>
-   );
+  const navListItems = navLinks.map((link, i) => {
+    // consider changing comparison to be based on something else such as slug or uid
+    if (link.text && link.text.toLowerCase() === "services") {
+      return (
+        <li key={`nav-link-${i}`} ref={serviceMenuRef}>
+          <ServicesMenu
+            linkDisplayText={link.text}
+            servicePageLinks={servicePageLinks}
+            setNavClosedAction={setNavClosed}
+            navButtonRef={navButtonRef}
+          />
+        </li>
+      );
+    } else {
+      return (
+        <li key={`nav-link-${i}`}>
+          <PrismicNextLink field={link} className={moduleStyles.navLink} />
+        </li>
+      );
+    }
+  });
+
+  return (
+    <nav className={moduleStyles.navContainer} aria-label="Main Navigation">
+      <MenuToggleButton
+        displayText="MENU"
+        menuOpen={navMenuOpen}
+        onToggle={toggleNavMenuOpenState}
+        buttonToggleRef={navButtonRef}
+        menuOnlyMobile={true}
+        arrowOnlyMobile={true}
+        ariaControlsId="nav-menu"
+        arrowSize="large"
+        arrowColor="brown-500"
+        className={moduleStyles.menuToggleButton}
+      />
+      <ul
+        id="nav-menu"
+        className={clsx(
+          moduleStyles.linksContainer,
+          navMenuOpen && moduleStyles.linksDisplayed,
+          !navMenuOpen && moduleStyles.linksHidden
+        )}
+      >
+        {navListItems}
+      </ul>
+    </nav>
+  );
 }
