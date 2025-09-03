94a8d2d25c1753e1ef80d2b354ef8bc5772c5ef8
Dalton Pettus <daltpettus@gmail.com>
2025-09-03 10:00:11 -0700

    remove conditional check that tsestimonials array must not be empty, threw hook errors

--- src/components/TestimonialCycler/TestimonialCycler.tsx
+++ src/components/TestimonialCycler/TestimonialCycler.tsx
@@ -14,9 +14,7 @@ export default function TestimonialCycler({
   testimonials,
   className,
 }: TestimonialCyclerProps) {
-  if (testimonials.length === 0) {
-    return null;
-  }
+  
 
   const [testimonial, setTestimonial] = useState({
     text: testimonials[0],
