54b882f692ca21f0b36950ecc3d66c140481a4ed
Dalton Pettus <daltpettus@gmail.com>
2025-09-03 15:38:47 -0700

    escape quotes in fallback testimonials

--- src/components/TestimonialCycler/fallbackTestimonials.tsx
+++ src/components/TestimonialCycler/fallbackTestimonials.tsx
@@ -2,23 +2,23 @@ import { JSX } from "react";
 
 const fallbackTestimonials: JSX.Element[] = [
   <p key="fallback-testimonial-1">
-    "Grace is absolutely amazing. By far, the only person I trust with my spray
+    &ldquo;Grace is absolutely amazing. By far, the only person I trust with my spray
     tans. It looks so natural and flawless, and she gives you all as long as
     possible, comfortable and welcome and suits your needs. If you need a spray
-    tan in San Diego Grace is your girl. Highly highly recommend Grace!"
+    tan in San Diego Grace is your girl. Highly highly recommend Grace!&rdquo;
   </p>,
   <p key="fallback-testimonial-2">
-    "I've been going to Grace for my tans for about a year now and every single
+    &ldquo;I&rsquo;ve been going to Grace for my tans for about a year now and every single
     time my tan has turned out flawless and so natural looking! She will help
     you decide the right tan for you & makes you feel so comfortable. I highly
     recommend grace to anyone looking to get a perfect, natural, long lasting
-    spray tan!"
+    spray tan!&rdquo;
   </p>,
   <p key="fallback-testimonial-3">
-    "Grace is amazing! Her tan stayed on for such a long time and looked great!
+    &ldquo;Grace is amazing! Her tan stayed on for such a long time and looked great!
     I got so many compliments saying how natural my tan looked for my bridal
     shower! She makes you feel so comfortable and I will definitely be using her
-    again for my wedding and big events!"
+    again for my wedding and big events!&rdquo;
   </p>,
 ];
 
