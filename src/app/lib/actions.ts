183567219e27ee59742e3238b5a8f7452fff233f
Dalton Pettus <daltpettus@gmail.com>
2025-09-03 19:29:48 -0700

    add min requirement to message

--- src/app/lib/actions.ts
+++ src/app/lib/actions.ts
@@ -215,7 +215,12 @@ const AppointmentRequestSchema = z.object({
       required_error: "Message is required",
       invalid_type_error: "Message must be a string",
     })
-    .max(500)
+    .min(10, {
+      message: "Message must be at least 10 characters long",
+    })
+    .max(500, {
+      message: "Message must be at most 500 characters long",
+    })
     .regex(/^$|[a-zA-Z0-9.,'"?:()_@#!&$\-\/ \n\r]+$/, {
       message:
         "Message may only contain letters, numbers, spaces, and the following special characters: . _ @ # ! & $ - /",
