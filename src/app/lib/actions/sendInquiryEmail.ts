e35b7cac3cfd2b1df80a67b373f0748d595458c4
Dalton Pettus <daltpettus@gmail.com>
2026-02-27 18:01:21 -0800

    handle mailgun apikey misconfiguration

--- src/app/lib/actions/sendInquiryEmail.ts
+++ src/app/lib/actions/sendInquiryEmail.ts
@@ -43,10 +43,19 @@ export async function sendInquiryEmail(
   prevState: InquiryState,
   formData: FormData,
 ): Promise<InquiryState> {
+  const apiKey = process.env.MAILGUN_API_KEY;
+  if (!apiKey) {
+    console.error("Mailgun API key is not properly configured.");
+    return {
+      message: "Please contact support.",
+      success: false,
+    };
+  }
+
   const mailgun = new Mailgun(FormData);
   const mg = mailgun.client({
     username: "api",
-    key: process.env.MAILGUN_API_KEY!,
+    key: apiKey,
   });
 
   const rawFormData = Object.fromEntries(formData);
