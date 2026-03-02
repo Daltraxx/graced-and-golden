821c3007886651b89b59b9ec3d3db28233eb1daf
Dalton Pettus <daltpettus@gmail.com>
2026-03-01 17:12:19 -0800

    add NewsletterSubscriptionSchema

---
+++ src/app/lib/schema/NewsletterSubscriptionSchema.ts
@@ -0,0 +1,16 @@
+import { z } from "zod";
+
+export const NewsletterSubscriptionSchema = z.object({
+  email: z
+    .string({
+      required_error: "Email is required",
+      invalid_type_error: "Email must be a string",
+    })
+    .email({
+      message: "Please enter a valid email address.",
+    }),
+});
+
+export type NewsletterSubscriptionState = z.infer<
+  typeof NewsletterSubscriptionSchema
+>;
