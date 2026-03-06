c1c0943e02c959a9833ac5754e11eb010475516a
Dalton Pettus <daltpettus@gmail.com>
2026-03-05 20:37:18 -0800

    remove fetching copy from prismic for now

--- src/components/CTAModal/Modal/CTAModal.tsx
+++ src/components/CTAModal/Modal/CTAModal.tsx
@@ -1,43 +1,26 @@
 import Heading from "@/components/Heading";
-import { createClient } from "@/prismicio";
-import { JSXMapSerializer, PrismicRichText } from "@prismicio/react";
 import styles from "./styles.module.css";
 import clsx from "clsx";
-
-const components: JSXMapSerializer = {
-  heading2: ({ children }) => (
-    <Heading as="h2" size="manual" font="display">
-      {children}
-    </Heading>
-  ),
-  heading3: ({ children }) => (
-    <Heading as="h3" size="manual" font="display">
-      {children}
-    </Heading>
-  ),
-  paragraph: ({ children }) => <p>{children}</p>,
-};
 
 type CTAModalProps = {
   isOpen: boolean;
   onClose: () => void;
 };
 
-export default async function CTAModal({ isOpen, onClose }: CTAModalProps) {
-  const client = createClient();
-  const copy = await client.getSingle("newsletter_cta");
-
+export default function CTAModal({ isOpen, onClose }: CTAModalProps) {
   return (
     <>
       <section className={clsx(styles.ctaModal, { [styles.open]: isOpen })}>
-        <PrismicRichText field={copy.data.heading} components={components} />
-        <PrismicRichText field={copy.data.body_text} components={components} />
+        <Heading as="h2" size="manual" font="display">
+          Subscribe to our Newsletter
+        </Heading>
+        <p>
+          Updates, exclusive deals, last minute openings & pro tips... straight
+          to your inbox.
+        </p>
         <form action="">
           <label htmlFor="email" className={styles.emailLabel}>
-            <PrismicRichText
-              field={copy.data.email_form_label}
-              components={components}
-            />
+            Enter your email:
           </label>
           <input type="email" id="email" name="email" />
           <button type="submit">Subscribe</button>
