32a4023a3a5aeaefaaebc8502ad189a00388a5b1
Dalton Pettus <daltpettus@gmail.com>
2026-03-06 15:33:35 -0800

    change to 5 seconds for modal delay

--- src/components/CTAModal/Provider/CTAModalProvider.tsx
+++ src/components/CTAModal/Provider/CTAModalProvider.tsx
@@ -12,7 +12,7 @@ export default function CTAModalProvider({
   useEffect(() => {
     const timer = setTimeout(() => {
       setIsOpen(true);
-    }, 3000); // Open after 3 seconds
+    }, 5000); // Open after 5 seconds
 
     return () => clearTimeout(timer); // Cleanup on unmount
   }, []);
