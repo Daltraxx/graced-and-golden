2795cb4ae3d2828ee953151782272a3d490ec163
Dalton Pettus <daltpettus@gmail.com>
2026-03-06 10:56:37 -0800

    remove unnecessary wrapping fragment

--- src/components/CTAModal/Modal/CTAModal.tsx
+++ src/components/CTAModal/Modal/CTAModal.tsx
@@ -9,28 +9,26 @@ type CTAModalProps = {
 
 export default function CTAModal({ isOpen, onClose }: CTAModalProps) {
   return (
-    <>
-      <section
-        className={clsx(styles.ctaModal, { [styles.open]: isOpen })}
-        role="dialog"
-        aria-modal="true"
-        aria-labelledby="cta-modal-heading"
-      >
-        <Heading as="h2" size="manual" font="display" id="cta-modal-heading">
-          Subscribe to our Newsletter
-        </Heading>
-        <p>
-          Updates, exclusive deals, last minute openings & pro tips... straight
-          to your inbox.
-        </p>
-        <form action="">
-          <label htmlFor="email" className={styles.emailLabel}>
-            Enter your email:
-          </label>
-          <input type="email" id="email" name="email" />
-          <button type="submit">Subscribe</button>
-        </form>
-      </section>
-    </>
+    <section
+      className={clsx(styles.ctaModal, { [styles.open]: isOpen })}
+      role="dialog"
+      aria-modal="true"
+      aria-labelledby="cta-modal-heading"
+    >
+      <Heading as="h2" size="manual" font="display" id="cta-modal-heading">
+        Subscribe to our Newsletter
+      </Heading>
+      <p>
+        Updates, exclusive deals, last minute openings & pro tips... straight to
+        your inbox.
+      </p>
+      <form action="">
+        <label htmlFor="email" className={styles.emailLabel}>
+          Enter your email:
+        </label>
+        <input type="email" id="email" name="email" />
+        <button type="submit">Subscribe</button>
+      </form>
+    </section>
   );
 }
