8cc8a46ffece4ee6c2b849277b05341363f724dc
Dalton Pettus <daltpettus@gmail.com>
2026-03-06 15:52:21 -0800

    add handleClose function

--- src/components/CTAModal/Provider/CTAModalProvider.tsx
+++ src/components/CTAModal/Provider/CTAModalProvider.tsx
@@ -9,6 +9,11 @@ export default function CTAModalProvider({
   children: React.ReactNode;
   }) {
   const [isOpen, setIsOpen] = useState(false);
+  const handleClose = () => {
+    setIsOpen(false);
+    // TODO: Consider setting a cookie or localStorage item here 
+    // to prevent showing the modal again for a certain period of time
+  };
   useEffect(() => {
     const timer = setTimeout(() => {
       setIsOpen(true);
@@ -16,10 +21,11 @@ export default function CTAModalProvider({
 
     return () => clearTimeout(timer); // Cleanup on unmount
   }, []);
+  
   return (
     <>
       {children}
-      <CTAModal isOpen={isOpen} onClose={() => setIsOpen(false)} />
+      <CTAModal isOpen={isOpen} onClose={handleClose} />
     </>
   );
 }
