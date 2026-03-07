601fb33510cbe8c3938913a042a87ae2a19aa597
Dalton Pettus <daltpettus@gmail.com>
2026-03-06 17:56:13 -0800

    hook up server action to form

--- src/components/CTAModal/Modal/CTAModal.tsx
+++ src/components/CTAModal/Modal/CTAModal.tsx
@@ -3,7 +3,17 @@ import { Dialog } from "radix-ui";
 import { Cross2Icon } from "@radix-ui/react-icons";
 import styles from "./styles.module.css";
 import { NonPrismicButton } from "@/components/Button/Button";
+
+import { subscribeToNewsletter } from "@/app/lib/actions/subscribeToNewsletter";
+import { NewsletterSubscriptionState } from "@/app/lib/schema/NewsletterSubscriptionSchema";
 import clsx from "clsx";
+import { useActionState } from "react";
+
+const INITIAL_SUBSCRIPTION_STATE = {
+  message: "",
+  errors: {},
+  success: false,
+} satisfies NewsletterSubscriptionState;
 
 type CTAModalProps = {
   isOpen: boolean;
@@ -11,6 +21,11 @@ type CTAModalProps = {
 };
 
 export default function CTAModal({ isOpen, onClose }: CTAModalProps) {
+  const [subscriptionState, formAction, isPending] = useActionState(
+    subscribeToNewsletter,
+    INITIAL_SUBSCRIPTION_STATE,
+  );
+
   return (
     <Dialog.Root open={isOpen} onOpenChange={onClose}>
       <Dialog.Portal>
@@ -24,18 +39,17 @@ export default function CTAModal({ isOpen, onClose }: CTAModalProps) {
             straight to your inbox.
           </Dialog.Description>
 
-          <form action="">
+          <form action={formAction} className={styles.form}>
             <label htmlFor="subscribe-email">Enter your email:</label>
             <input type="email" id="subscribe-email" name="email" />
-            <Dialog.Close asChild>
-              <NonPrismicButton
-                color="olive-brown-700"
-                className={styles.submitButton}
-                type="submit"
-              >
-                Submit
-              </NonPrismicButton>
-            </Dialog.Close>
+            <NonPrismicButton
+              color="olive-brown-700"
+              className={styles.submitButton}
+              type="submit"
+              disabled={isPending}
+            >
+              Submit
+            </NonPrismicButton>
           </form>
           <Dialog.Close asChild>
             <button className={styles.IconButton} aria-label="Close">
