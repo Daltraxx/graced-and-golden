26562dd0e717f5259c4ad1e9eb4b21a6d11f8fec
Dalton Pettus <daltpettus@gmail.com>
2026-03-13 17:17:14 -0700

    use EmailSchema in NewsletterSubscriptionSchema

--- src/app/lib/schema/NewsletterSubscriptionSchema.ts
+++ src/app/lib/schema/NewsletterSubscriptionSchema.ts
@@ -1,17 +1,8 @@
 import { z } from "zod";
+import { EmailSchema } from "./EmailSchema";
 
 export const NewsletterSubscriptionSchema = z.object({
-  email: z
-    .string({
-      required_error: "Email is required",
-      invalid_type_error: "Email must be a string",
-    })
-    .trim()
-    .min(5, "Email must be at least 5 characters long")
-    .max(254, "Email must be at most 254 characters long")
-    .email({
-      message: "Please enter a valid email address.",
-    }),
+  email: EmailSchema
 });
 
 export type NewsletterSubscriptionState = {
