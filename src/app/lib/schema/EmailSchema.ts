722baa868dc114f95beccff474d6b52dce855003
Dalton Pettus <daltpettus@gmail.com>
2026-03-13 17:21:41 -0700

    change emailschema to field schema

--- src/app/lib/schema/EmailSchema.ts
+++ src/app/lib/schema/EmailSchema.ts
@@ -1,15 +1,13 @@
 import { z } from "zod";
 
-export const EmailSchema = z.object({
-  email: z
-    .string({
-      required_error: "Email is required",
-      invalid_type_error: "Email must be a string",
-    })
-    .trim()
-    .min(5, "Email must be at least 5 characters long")
-    .max(254, "Email must be at most 254 characters long")
-    .email({
-      message: "Please enter a valid email address.",
-    }),
-});
+export const EmailSchema = z
+  .string({
+    required_error: "Email is required",
+    invalid_type_error: "Email must be a string",
+  })
+  .trim()
+  .min(5, "Email must be at least 5 characters long")
+  .max(254, "Email must be at most 254 characters long")
+  .email({
+    message: "Please enter a valid email address.",
+  });
