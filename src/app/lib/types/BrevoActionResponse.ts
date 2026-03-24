143f4b5320abb3dbf40e391f157353f83099d7f9
Dalton Pettus <daltpettus@gmail.com>
2026-03-23 19:13:25 -0700

    simplify doc

--- src/app/lib/types/BrevoActionResponse.ts
+++ src/app/lib/types/BrevoActionResponse.ts
@@ -1,9 +1,8 @@
 /**
- * Represents the response from a helper function that interacts with the Brevo API.
- * @typedef {Object} BrevoActionResponse
- * @property {boolean} success - Indicates whether the action was completed successfully.
- * @property {string} message - A descriptive message about the action result or any error that occurred.
- * @property {number} [code] - Optional HTTP status code or error code associated with the response.
+ * Represents the response from a Brevo action.
+ * @property {boolean} success - Indicates whether the Brevo action was successful.
+ * @property {string} message - A descriptive message about the action result.
+ * @property {number} [code] - Optional status or error code associated with the response.
  */
 export type BrevoActionResponse = {
   success: boolean;
